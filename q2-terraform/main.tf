d42c539c46bd45c18f624d2b6ad45b62127c8f72
SadafRiaz077 <sadafriaz184@gmail.com>
2026-01-19 12:55:23 +0000

    Add Q2 Terraform files and .gitignore

---
+++ q2-terraform/main.tf
@@ -0,0 +1,128 @@
+
+provider "aws" {
+  region  = "me-central-1"
+  profile = "default"
+}
+
+
+resource "aws_vpc" "myapp_vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "myapp_subnet" {
+  vpc_id            = aws_vpc.myapp_vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "myapp_igw" {
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+resource "aws_default_route_table" "myapp_rt" {
+  default_route_table_id = aws_vpc.myapp_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp_igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rt"
+  }
+}
+
+data "http" "my_ip" {
+  url = "https://icanhazip.com"
+}
+
+locals {
+  my_ip = "${chomp(data.http.my_ip.response_body)}/32"
+}
+
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [local.my_ip]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+resource "aws_key_pair" "serverkey" {
+  key_name   = "serverkey"
+  public_key = file("/home/codespace/.ssh/id_ed25519.pub")
+}
+
+data "aws_ami" "amazon_linux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+resource "aws_instance" "myapp_ec2" {
+  ami                         = data.aws_ami.amazon_linux2.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.myapp_subnet.id
+  availability_zone           = var.availability_zone
+  vpc_security_group_ids      = [aws_default_security_group.default_sg.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.serverkey.key_name
+  user_data                   = file("entry-script.sh")
+
+  tags = {
+    Name = "${var.env_prefix}-ec2-instance"
+  }
+}
+
